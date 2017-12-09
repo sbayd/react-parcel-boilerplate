1e65da8265aac3a77c6b48a9efeb58f602075011
Berkay Aydin <sbayd06@gmail.com>
2017-12-09 22:56:52 +0300

    support and use spread operator for objects

--- src/reducers/ui.js
+++ src/reducers/ui.js
@@ -1,13 +1,12 @@
 // SIMPLE REDUCER
 import { UI_ACTIONS } from '../constants/action_types';
-import assign from 'lodash/assign';
 
 export default (state = { count: 1, userName: 'Berkay Aydin', webSite: 'http://sbaydin.com' }, action) => {
   switch (action.type) {
   case UI_ACTIONS.UPDATE_NAME:
-    return assign({} ,state, { userName: action.data });
+    return { ...state, userName: action.data };
   case UI_ACTIONS.INCREMENT_COUNT:
-    return assign({} ,state, { count: action.data });
+    return { ...state, count: action.data };
   default:
     return state;
   }
